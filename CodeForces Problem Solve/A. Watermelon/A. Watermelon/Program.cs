6ff1445324f021dea67cfabe0ed492bfd83d54d6
Sabbir Ahmed <sabbirahmed628544@gmail.com>
2021-04-21 10:49:54 +0600

    A. Watermelon added

---
+++ CodeForces Problem Solve/A. Watermelon/A. Watermelon/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace A._Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            if (w > 2 && w % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
+
