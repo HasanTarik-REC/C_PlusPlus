238ef70fe52450ebd114a6328b4a24b0a47f6528
Sabbir Ahmed <sabbirahmed628544@gmail.com>
2021-04-21 12:37:52 +0600

    A. Way Too Long Words added

---
+++ CodeForces Problem Solve/A. Way Too Long Words/A. Way Too Long Words/A. Way Too Long Words/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A._Way_Too_Long_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, count = 0;
+            n = int.Parse(Console.ReadLine());
+            while (count != n)
+            {
+                var Array1 = Console.ReadLine();
+                if (Array1.Length > 10)
+                {
+                    int x = Array1.Length;
+                    Console.Write(Array1[Array1.Length - x]);
+                    Console.Write(x-2);
+                    Console.WriteLine(Array1[Array1.Length-1]);
+                }
+                else
+                    Console.WriteLine(Array1);
+                count++;
+            }
+        }
+    }
+}
