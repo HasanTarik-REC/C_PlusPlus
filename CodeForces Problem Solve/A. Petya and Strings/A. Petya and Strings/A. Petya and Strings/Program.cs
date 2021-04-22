6aafb4f279406b3f5bd28cd48dc4ae93e8f656fd
Sabbir Ahmed <sabbirahmed628544@gmail.com>
2021-04-22 17:50:35 +0600

    A. Petya and Strings added

---
+++ CodeForces Problem Solve/A. Petya and Strings/A. Petya and Strings/A. Petya and Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A._Petya_and_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FirstString = Console.ReadLine();
+            var firstString=FirstString.ToLower();
+            var SecondString = Console.ReadLine();
+            var secondString = SecondString.ToLower();
+            var result=firstString.CompareTo(secondString);
+            Console.WriteLine(result);
+
+        }
+    }
+}
